2905c3920064752f71230664baab388e3d3cea3e
Boncho Belutov <bbelutov@axway.com>
2018-07-20 00:55:34 +0300

    changed the file structure | added Persons, ErrorBoundery and Cockpit components

---
+++ src/containers/App.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import classes from "./App.css";
+import Persons from "../components/Persons/Persons";
+import Cockpit from "../components/Cockpit/Cockpit";
+
+class App extends Component {
+  state = {
+    persons: [
+      { id: "1", name: "Max", age: 29 },
+      { id: "2", name: "Mashala", age: 19 },
+      { id: "3", name: "Mara", age: 9 }
+    ],
+    showPersons: false
+  };
+
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState({
+      persons
+    });
+  };
+
+  deletePersonsHandler = personIndex => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({
+      persons
+    });
+  };
+
+  togglePersonsHandler = () => {
+    const showPersons = this.state.showPersons;
+    this.setState({
+      showPersons: !showPersons
+    });
+  };
+
+  render() {
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonsHandler}
+          changed={this.nameChangeHandler}
+        />
+      );
+    }
+
+    return (
+      <div className={classes.App}>
+        <Cockpit
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          clicked={this.togglePersonsHandler}
+        />
+        {persons}
+      </div>
+    );
+  }
+}
+
+export default App;
